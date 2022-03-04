ab9b458390368e19bf5039800896e6834dcf5aa0
pb-aizawa <shuntaxiangze45@gmail.com>
2022-03-04 15:58:32 +0900

    toggle menu 追加('master')

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -1,47 +1,56 @@
 $(function () {
     $("#profile").hover(function () {
         $("#profile").text("プロフィール");
-    }, function(){
+    }, function () {
         $("#profile").text("PROFILE");
     });
 });
 
-$(function () {
-    $("#prodacts").hover(function () {
-        $("#prodacts").text("成果物");
-    }, function(){
-        $("#prodacts").text("PRODACTS");
+// $(function () {
+//     $("#prodacts").hover(function () {
+//         $("#prodacts").text("成果物").on("click", function () {
+//             $(".is-open").slideToggle();
+//         });
+//     }, function () {
+//         $("#prodacts").text("PRODACTS");
+//     });
+// });
+$(function(){
+    $("#prodacts").on("click", function(){
+        $(".is-open").slideToggle();
     });
-});
+})
+
 
 $(function () {
     $("#github").hover(function () {
         $("#github").text("https://github.com/Shunta-422/html-practice")
-        .css(p_props);
-    }, function(){
+            .css(p_props);
+    }, function () {
         $("#github").text("Github")
-        .css(init_set);
+            .css(init_set);
     });
 });
 
 $(function () {
     $("#qiita").hover(function () {
         $("#qiita").text("https://qiita.com/aizawa_PG")
-        .css(p_props);
-    }, function(){
+            .css(p_props);
+    }, function () {
         $("#qiita").text("Qiita")
-        .css(init_set);
+            .css(init_set);
     });
 });
 var init_set = {
-    "color" :"palevioletred",
-    "font-size" : "30px",
-    "font-weight" : "300",
-    "letter-spacing" :".4em"
+    "color": "palevioletred",
+    "font-size": "30px",
+    "font-weight": "300",
+    "letter-spacing": ".4em"
 }
 var p_props = {
-    "color" : "deeppink",
-    "font-size" : "15px",
-    "font-weight" : "100",
-    "letter-spacing" : "0.2em"
+    "color": "deeppink",
+    "font-size": "15px",
+    "font-weight": "100",
+    "letter-spacing": "0.2em"
 }
+
