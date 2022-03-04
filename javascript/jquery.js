5b5141256151db32b22bd9acb5a5bebe692a3044
pb-aizawa <shuntaxiangze45@gmail.com>
2022-03-04 15:25:36 +0900

    jquery追加('master')

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -1,3 +1,47 @@
-$("portfolio").hover(function(){
-    $("portfolio").html("ポートフォリオ");
+$(function () {
+    $("#profile").hover(function () {
+        $("#profile").text("プロフィール");
+    }, function(){
+        $("#profile").text("PROFILE");
+    });
 });
+
+$(function () {
+    $("#prodacts").hover(function () {
+        $("#prodacts").text("成果物");
+    }, function(){
+        $("#prodacts").text("PRODACTS");
+    });
+});
+
+$(function () {
+    $("#github").hover(function () {
+        $("#github").text("https://github.com/Shunta-422/html-practice")
+        .css(p_props);
+    }, function(){
+        $("#github").text("Github")
+        .css(init_set);
+    });
+});
+
+$(function () {
+    $("#qiita").hover(function () {
+        $("#qiita").text("https://qiita.com/aizawa_PG")
+        .css(p_props);
+    }, function(){
+        $("#qiita").text("Qiita")
+        .css(init_set);
+    });
+});
+var init_set = {
+    "color" :"palevioletred",
+    "font-size" : "30px",
+    "font-weight" : "300",
+    "letter-spacing" :".4em"
+}
+var p_props = {
+    "color" : "deeppink",
+    "font-size" : "15px",
+    "font-weight" : "100",
+    "letter-spacing" : "0.2em"
+}
